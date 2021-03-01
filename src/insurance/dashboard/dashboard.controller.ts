3705337719ca8d8105eb2acbb322df34929dc483
Liuver Duran <liuver.duran@solarisoilfield.com>
2021-02-28 19:50:46 -0600

    implemented bulk charts generation

--- src/insurance/dashboard/dashboard.controller.ts
+++ src/insurance/dashboard/dashboard.controller.ts
@@ -1,7 +1,9 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
+  Post,
   Query,
   Req,
   Response,
@@ -15,6 +17,7 @@ import { RoleType } from 'shared/enum/role-type.enum';
 import { HasRoles } from 'auth/guard/has-roles.decorator';
 import { User } from 'database/user.model';
 import { Request } from 'express';
+import { randomBytes } from 'crypto';
 
 @Controller({ path: 'dashboards', scope: Scope.REQUEST })
 export class DashboardController {
@@ -64,49 +67,75 @@ export class DashboardController {
     return res.json(response);
   }
 
-  @Get('sales/line')
+
+  @Post('sales/batch')
   @HttpCode(200)
   @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRoles(RoleType.USER, RoleType.ADMIN)
-  async getSalesLineChart(
+  async getSalesCharts(
     @Req() req: Request,
     @Response() res,
-    @Query('data_criteria') dataCriteria: string,
-    @Query('grouping_criteria') groupingCriteria: string,
-    @Query('aggregation') aggregation: string,
-    @Query('location') location?: string,
-    @Query('start_date') startDate?: string,
-    @Query('end_date') endDate?: string,
-    @Query('options') options?: string,
+    @Body() body,
   ): Promise<any> {
 
     const user: Partial<User> = req.user;
 
-    const aggregations = await this.dashboardService.getSalesBy(dataCriteria, groupingCriteria, aggregation, user, startDate, endDate, location);
+    const queries = body.hasOwnProperty('queries') && body['queries'];
+
+    
+
+    if (queries.length) {
+
+      Promise.all(queries.map(async config => {
+
+        switch (config.type) {
+          case "bar":
+            const aggregations = await this.dashboardService.getSalesBy(config.queryParams.dataCriteria, config.queryParams.groupingCriteria, config.queryParams.aggregation,
+              user, config.queryParams.startDate, config.queryParams.endDate, config.queryParams.location);
+
+            aggregations.sort(function (a, b) {
+              return ((a._id.label < b._id.label) ? -1 : ((a._id.label == b._id.label) ? 0 : 1));
+            });
+
+            const salesValues = aggregations.map((aggregation) => (Math.round(aggregation.data)));
+            const salesLabels = aggregations.map((aggregation) => (aggregation._id.label));
+
+            const response = {
+              type: config.type,
+              data: {
+                datasets: [{
+                  data: salesValues,
+                  //backgroundColor: []
+                }],
+                labels: salesLabels
+              },
+              options: {}
+            }
+
+            console.log(response);
+
+            return response;
+            
+            break;
+
+          default:
+            break;
+        }
 
 
-    aggregations.sort(function (a, b) {
-      return ((a._id.label < b._id.label) ? -1 : ((a._id.label == b._id.label) ? 0 : 1));
-    });
 
-    const salesValues = aggregations.map((aggregation) => (Math.round(aggregation.data)));
-    const salesLabels = aggregations.map((aggregation) => (aggregation._id.label));
+      })).then(data => {
+        return res.json(data);
+      });
 
-
-    const response = {
-      type: "line",
-      data: {
-        datasets: [{
-          data: salesValues,
-          //backgroundColor: []
-        }],
-        labels: salesLabels
-      },
-      options: {}
     }
 
-    return res.json(response);
+
+
+    
   }
+
+
 
 
 }
