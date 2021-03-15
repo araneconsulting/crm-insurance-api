722834bb6d0a75abdbe27ae70dcbadbab8e660e7
Liuver Duran <liuver@gmail.com>
2021-03-14 22:56:28 -0500

    added hot-reload

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,5 +14,11 @@ async function bootstrap() {
   app.enableCors();
   //app.useLogger();
   await app.listen(5000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
 }
 bootstrap();
