9e365cac4ea8c7c958745d0d2839d6a7c0194915
Liuver Duran <liuver@gmail.com>
2021-03-19 22:45:17 -0500

    configured env config and db

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongodb', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb://localhost/arane_insurance_crm',
+  uri: process.env.MONGODB_URI,
 }));
