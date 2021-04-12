fc6683233b7fefa1b3a1ddea001bbbf001e051c2
Liuver Duran <liuver@gmail.com>
2021-04-12 18:59:23 -0500

    <partial with error> improve user model

---
+++ src/auth/interface/authenticated-user.interface.ts
@@ -0,0 +1,13 @@
+import { RoleType } from '../../shared/enum/role-type.enum';
+
+export interface AuthenticatedUser {
+  readonly username: string;
+  readonly firstName: string,
+  readonly lastName: string,
+  readonly id: string;
+  readonly email: string;
+  readonly roles: RoleType[];
+  readonly position: string;
+  readonly location: string;
+  readonly phone: string;
+}
