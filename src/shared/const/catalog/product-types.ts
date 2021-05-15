692a2207370d8dd7f87ef62e343fcbfe017b0c98
Liuver Duran <liuver@gmail.com>
2021-05-15 11:05:03 -0500

    add product names to product types catalog

--- src/shared/const/catalog/product-types.ts
+++ src/shared/const/catalog/product-types.ts
@@ -1,11 +1,52 @@
 export const productTypes = [
-  { id: 'TRUCKING', name: 'Commercial Trucking Insurance' },
-  { id: 'AUTO', name: 'Auto Insurance' },
-  { id: 'HOME', name: 'Homeowner Insurance' },
-  { id: 'RENTAL', name: 'Rental Insurance' },
-  { id: 'COMMERCIAL', name: 'Commercial Insurance' },
-  { id: 'LIFE', name: 'Life insurance' },
-  { id: 'HEALTH', name: 'Health Insurance' },
-  { id: 'PERMIT', name: 'Permits' },
-
+  {
+    id: 'TRUCKING',
+    name: 'Commercial Trucking Insurance',
+    products: [
+      { id: 'TRUCKING_LIABILITY', name: 'General Liability' },
+      { id: 'TRUCKING_PHYSICAL_DAMAGE', name: 'Physical Damage' },
+      { id: 'TRUCKING_WCGLUMB', name: 'WC/GL/Umbrella' },
+      { id: 'TRUCKING_CARGO', name: 'Cargo Damage' },
+      { id: 'TRUCKING_PERMIT', name: 'Permit' },
+    ],
+  },
+  {
+    id: 'AUTO',
+    name: 'Auto Insurance',
+    products: [
+      { id: 'AUTO_LIABILITY_FULL', name: 'Full Coverage Liability' },
+      { id: 'AUTO_LIABILITY_GENERAL', name: 'General Liability' },
+      { id: 'AUTO_GAP', name: 'GAP' },
+    ],
+  },
+  {
+    id: 'HOME',
+    name: 'Homeowner Insurance',
+    products: [{ id: 'HOME_FLOOD', name: 'Flood Coverage' }],
+  },
+  {
+    id: 'RENTAL',
+    name: 'Rental Insurance',
+    products: [{ id: 'RENTAL_ELECTRONICS', name: 'Electronics' }],
+  },
+  {
+    id: 'COMMERCIAL',
+    name: 'Commercial Insurance',
+    products: [{ id: 'COMMERCIAL_FLOOD', name: 'Flood Coverage' }],
+  },
+  {
+    id: 'LIFE',
+    name: 'Life insurance',
+    products: [{ id: 'LIFE_LIABILITY_BASIC', name: 'Basic Liability' }],
+  },
+  {
+    id: 'HEALTH',
+    name: 'Health Insurance',
+    products: [{ id: 'HEALTH_LIABILITY_BASIC', name: 'Basic Liability' }],
+  },
+  {
+    id: 'PERMIT',
+    name: 'Permits',
+    products: [{ id: 'PERMIT_COMPANY', name: 'Company Permit' }],
+  },
 ];
